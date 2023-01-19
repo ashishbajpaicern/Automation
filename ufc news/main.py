8186a5492f879067fb2878237a7205be48c290c5
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2023-01-19 23:08:53 +0530

    updates

--- ufc news/main.py
+++ ufc news/main.py
@@ -3,31 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://www.espn.in/mma/"
+url = "https://www.espn.com/mma/news"
 data = []
 
 while True:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # Find all the news headlines on the page
-    headlines = soup.find_all("h1", class_="headline")[
-        :5]  # Get top 5 headlines
+    # Find all the news articles on the page
+    articles = soup.find_all("p", string=lambda text: "UFC" in text)
 
-    # Extract the text of each headline and store them in data
-    for headline in headlines:
-        data.append(headline.text)
+    # Extract the text of each article and store them in data
+    for article in articles:
+        data.append(article.text)
 
     # dump data to json file
-    with open("ufc_news.json", "w") as f:
+    with open("ufc_articles.json", "w") as f:
         json.dump(data, f)
 
     # load data from json file
-    with open("ufc_news.json", "r") as f:
+    with open("ufc_articles.json", "r") as f:
         json_data = json.load(f)
 
     # print json data
-    print(json_data)
+    print(json.dumps(json_data, indent=4))
 
     # delay for 10 seconds before making the next request
     time.sleep(10)
